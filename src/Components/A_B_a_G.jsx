a0f502d31d66267e8af91fcf71e9bbbd0268aa09
Bek023 <tolmasivich.mansur@gmail.com>
2025-04-26 20:35:16 +0500

    array of components

---
+++ src/Components/A_B_a_G.jsx
@@ -0,0 +1,9 @@
+
+
+export const A_B_a_G = () => {
+    return (
+        <div>
+            <h1>A_B_a_G</h1>
+        </div>
+    )
+}
--- src/utils/zustand.jsx
+++ src/utils/zustand.jsx
@@ -3,10 +3,22 @@ import axios from './axios';
 
 // COMPONENT LIST (kerak bo‘lsa ko‘paytirish mumkin)
 import { Profile } from '../Components/Profile';
+import { Main } from '../Components/Main';
+import { A_B_a_G } from '../Components/A_B_a_G';
+import {Meals} from '../Components/Meals';
+import{Company} from '../Components/Company';
+import {Love_story} from '../Components/Love_Story';
+import {Gallery} from '../Components/Gallery';
 
 const componentList = [
   { id: 1, title: 'Profile', component: () => <Profile /> },
-  // { id: 2, title: 'SomethingElse', component: <AnotherComponent /> },
+  { id: 2, title: 'Main', component: () => <Main /> },
+  { id: 3, title: 'A_B_a_G', component: () => <A_B_a_G /> },
+  { id: 4, title: 'Love_story', component: () => <Love_story /> },
+  { id: 5, title: 'Gallery', component: () => <Gallery /> },
+  { id: 6, title: 'Meals', component: () => <Meals /> },
+  { id: 7, title: 'Company', component: () => <Company /> },
+  
 ];
 
 // 🧩 Component Store
